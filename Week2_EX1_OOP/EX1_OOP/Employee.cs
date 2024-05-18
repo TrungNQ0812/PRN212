78979f0d9ecec94443d1adc5eb6e000409380e7c
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2024-05-18 12:39:05 +0700

    create class
    sdasdasd

---
+++ Week2_EX1_OOP/EX1_OOP/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1_OOP
+{
+    internal class Employee
+    {
+        string name;
+        int paymentPerHour;
+    }
+}
---
+++ Week2_EX1_OOP/EX1_OOP/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1_OOP
+{
+    public interface IEmployee
+    {
+        public int calculateSalary()
+        {
+        }
+
+        public string getName()
+        {
+        }
+    }
+}
