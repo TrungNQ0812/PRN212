9e8ffcc6dc0a9d65cbbd9fe85f22042775e5a984
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2024-06-03 22:28:10 +0700

    Create demo winform

---
+++ Winform_Demo/AutomobieSolution/AutomobieLibrary/DataAccess/CarDBContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomobieLibrary.BussinessObject;
+namespace AutomobieLibrary.DataAccess
+{
+    public class CarDBContext
+    {
+        //Initialize car list
+        private static List<Car> CarList = new List<Car>()
+        {
+            new Car{
+                CarID = 1, CarName="CRV", Manufacturer ="Honda",
+                Price = 30000, ReleaseYear = 2021 },
+            new Car{
+                CarID = 2, CarName = "Ford Focus", Manufacturer = "Ford",
+                Price = 15000, ReleaseYear = 2020}
+        };
+    }
+    //Using Singleton Pattern
+    private static CarDBContext instance = null;
+    private static readonly object instanceLock = new object();
+    private CarDBContext() { }
+    public static CarDBContext Instance
+    {
+        get
+        {
+            lock(instanceLock){
+                if (instance == null)
+                {
+                    instance = new CarDBContext();
+                }
+                return instance;
+            }
+        }
+    }
+    //--------------------------
+    public List<Car> GetCarList => CarList;
+    //--------------------------
+    public Car GetCarByID(Car car)
+    {
+        //Using LINQ to Object
+        Car car = CarList.SingleOrDefault(pro => pro, CarID);
+    }
+
+    //----------------------------------------
+    //add new a car
+    public void addNew(Car car)
+    {
+        Car pro = getCar
+    }
+}   
