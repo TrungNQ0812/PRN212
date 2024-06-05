b7a673e8ab7c1c58ee7dad4478ce8897e6b8e808
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2024-06-05 21:45:30 +0700

    myshop

---
+++ MyShopEcercise/ManageCategoriesApp/ManageCategoriesApp/ManageCategories.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageCategoriesApp
+{
+    public record Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    public class ManageCategories
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string ConnectionString = "Server=MSI\\TRUNGNQ;uid=sa;pwd=123;database=MyStore";
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "select CategoryID, CategoryName from Categories";
+            command = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                if (reader.HasRows == true)
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Category
+                        {
+                            CategoryID = reader.GetInt32("CategoryID"),
+                            CategoryName = reader.GetString("CategoryName")
+                        });
+                    }//end while
+                }//end if
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return categories;
+        }//end GetCategories
+        //-------------------------------------------
+        public void InsertCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            //CategoryID is Identity
+            command = new SqlCommand("Insert Categories values (@CategoryName)", connection);
+        }
+
+    }
+}
