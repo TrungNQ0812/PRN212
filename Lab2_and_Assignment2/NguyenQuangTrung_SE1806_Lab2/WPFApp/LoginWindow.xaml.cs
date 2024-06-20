a079872b5071716b5a4847e6bea4e728e1fcbebf
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2024-06-20 18:30:04 +0700

    update somethings

---
+++ Lab2_and_Assignment2/NguyenQuangTrung_SE1806_Lab2/WPFApp/LoginWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Services;
+using BusinessObjects.Models;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private readonly IAccountService iAccountService;
+        public LoginWindow()
+        {
+            iAccountService = new AccountService();
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            AccountMember account = iAccountService.GetAccountByID(txtUser.Text);
+            if (account != null && account.MemberPassword.Equals(txtPass.Password) && account.MemberRole == 1)
+            {
+                this.Hide();
+                MainWindow main = new MainWindow();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("You are not Permission!");
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab2_and_Assignment2/NguyenQuangTrung_SE1806_Lab2/WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BusinessObjects.Models;
+using Services;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+     public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void dgData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cboCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtUnitsInStock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtProductName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtProductID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
